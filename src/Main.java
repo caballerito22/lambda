05053a4fab9e10456351259c319b9eec83da9ca0
alecabnun <avestruz2265@gmail.com>
2024-09-19 12:04:04 +0200

    si

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().start();
+    }
+
+    public void start() {
+
+
+        JButton button1 = new JButton("Mega Button1");
+        add(button1);
+
+        JButton button2 = new JButton("Mega Button2");
+        add(button2);
+
+        JButton button3 = new JButton("Mega Button3");
+        add(button3);
+
+        setSize(600, 200);
+        setLayout(new GridLayout(1, 3));
+        setVisible(true);
+
+
+        button1.addActionListener(e -> System.out.println("Button1"));
+
+        ActionListener accion = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(e.getActionCommand());
+            }
+        };
+        button2.addActionListener(accion);
     }
 }
